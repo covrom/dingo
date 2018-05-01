415044f40d25f348a313dccef306436fb9a90f67
Roman TSovanyan <covrom@gmail.com>
2018-05-01 13:03:26 +0300

    small fix

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	portPtr := flag.String("port", "8000", "The port number to listen to.")
-	dbFilePathPtr := flag.String("database", "blog.db", "The database file path for Djingo to use.")
+	dbFilePathPtr := flag.String("database", "blog.db", "The database file path to use.")
 	privKeyPathPtr := flag.String("priv-key", "blog.rsa", "The private key file path for JWT.")
 	pubKeyPathPtr := flag.String("pub-key", "blog.rsa.pub", "The public key file path for JWT.")
 	flag.Parse()
