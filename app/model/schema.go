d9ba38964b6324451e7dbd28ea2a45a493da9ea2
Roman TSovanyan <covrom@gmail.com>
2018-05-04 17:07:10 +0300

    schema

--- app/model/schema.go
+++ app/model/schema.go
@@ -2,8 +2,6 @@ package model
 
 import "github.com/globalsign/mgo"
 
-// TODO: Autoincrement id | _id fields
-
 var DBName = "dingoblog"
 
 type shema_struct struct {
@@ -20,40 +18,19 @@ var shema_indexes = []shema_struct{
 		Sparse:     true,
 	}},
 	shema_struct{"comments", mgo.Index{
-		Key:        []string{"Id"},
-		Unique:     true,
-		DropDups:   true,
-		Background: true,
-		Sparse:     true,
-	}},
-	shema_struct{"comments", mgo.Index{
 		Key: []string{"Parent"},
 	}},
 	shema_struct{"comments", mgo.Index{
 		Key: []string{"PostId", "Parent", "Approved"},
 	}},
 	shema_struct{"messages", mgo.Index{
-		Key:        []string{"Id"},
-		Unique:     true,
-		DropDups:   true,
-		Background: true,
-		Sparse:     true,
-	}},
-	shema_struct{"messages", mgo.Index{
 		Key: []string{"IsRead"},
-	}},
-	shema_struct{"posts", mgo.Index{
-		Key:        []string{"id"},
-		Unique:     true,
-		DropDups:   true,
-		Background: true,
-		Sparse:     true,
 	}},
 	shema_struct{"posts", mgo.Index{
 		Key: []string{"slug"},
 	}},
 	shema_struct{"posts", mgo.Index{
-		Key: []string{"id", "is_page", "published"},
+		Key: []string{"_id", "is_page", "published"},
 	}},
 
 	shema_struct{"posts_tags", mgo.Index{
@@ -62,9 +39,8 @@ var shema_indexes = []shema_struct{
 	shema_struct{"posts_tags", mgo.Index{
 		Key: []string{"tag_id"},
 	}},
-	
+
 	shema_struct{"tags", mgo.Index{
 		Key: []string{"slug"},
 	}},
-
 }
